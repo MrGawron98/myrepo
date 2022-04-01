a30031a97c2748ef07eaa74d8983aee2eed31e88
MrGawron <55848064+MrGawron@users.noreply.github.com>
2022-04-01 18:31:32 +0200

    added function f1

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ def say_hello(name):
     print('hello {0}'.format(name))
     print('+++++++++')
 
-
 def sum_two_numbers(a, b):
     result = a + b
     print(result)
@@ -10,4 +9,8 @@ def sum_two_numbers(a, b):
 def subtract_two_numbers(a, b):
     result = a + b
     print(result)
+
+
+def f1():
+    print('f1')
 
